411206f4e84ad5a1abc12bd902120e19b541c66f
Satannyx <eric.john9ne@gmail.com>
2024-09-09 15:42:41 +0800

    updated border radius on salesmanagement

--- src/components/SalesManagement.js
+++ src/components/SalesManagement.js
@@ -6,26 +6,35 @@ import LowDemandItems from './LowDemandItems';
 
 const SalesManagement = () => {
     return (
-        <div className="p-4 h-screen flex flex-col gap-4">
-            <div className="flex flex-col lg:flex-row gap-4">
+        <div className="h-screen flex flex-col gap-4">
+            {/* Container for Sales Graph and Recent Orders */}
+            <div className="flex flex-col lg:flex-row gap-4 h-1/2">
                 {/* Sales Graph taking up full width on small screens and 2/3 on large screens */}
-                <div className="flex-1 lg:flex-[2] p-0 h-full">
-                    <SalesGraph />
+                <div className="flex-1 lg:flex-[2] bg-white shadow-md rounded-lg">
+                    <div className="p-4 h-full">
+                        <SalesGraph />
+                    </div>
                 </div>
 
                 {/* Recent Orders beside the graph */}
-                <div className="flex-1 lg:flex-[1] p-0 h-full">
-                    <RecentOrders />
+                <div className="flex-1 lg:flex-[1] bg-white shadow-md rounded-lg h-full">
+                    <div className="p-4 h-full">
+                        <RecentOrders />
+                    </div>
                 </div>
             </div>
 
-            {/* High Demand Items and Low Demand Items below the graph and recent orders */}
-            <div className="flex flex-col lg:flex-row gap-4 flex-grow">
-                <div className="flex-1 p-0 h-full">
-                    <HighDemandItems />
+            {/* Container for High Demand Items and Low Demand Items */}
+            <div className="flex flex-col lg:flex-row gap-4 h-1/2">
+                <div className="flex-1 bg-white shadow-md rounded-lg h-full">
+                    <div className="p-4 h-full">
+                        <HighDemandItems />
+                    </div>
                 </div>
-                <div className="flex-1 p-0 h-full">
-                    <LowDemandItems />
+                <div className="flex-1 bg-white shadow-md rounded-lg h-full">
+                    <div className="p-4 h-full">
+                        <LowDemandItems />
+                    </div>
                 </div>
             </div>
         </div>
